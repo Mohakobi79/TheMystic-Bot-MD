0134060fca247a707322c8b4e297ff5aba2d9680
Mohakobi79 <mohakobi79@gmail.com>
2024-08-05 20:48:35 +0100

    Update herramientas-translate.js

--- plugins/herramientas-translate.js
+++ plugins/herramientas-translate.js
@@ -1,36 +1,30 @@
-import translate from '@vitalets/google-translate-api';
-import fetch from 'node-fetch';
-
-
-const handler = async (m, {args, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language
-  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.herramientas__translate
-
-  const msg = `${tradutor.texto1[0]} ${usedPrefix + command} ${tradutor.texto1[1]}\n*${usedPrefix + command} ${tradutor.texto1[2]}\n*- https://cloud.google.com/translate/docs/languages*`;
-  if (!args || !args[0]) return m.reply(msg);
-  let lang = args[0];
-  let text = args.slice(1).join(' ');
-  const defaultLang = 'es';
-  if ((args[0] || '').length !== 2) {
-    lang = defaultLang;
-    text = args.join(' ');
-  }
-  if (!text && m.quoted && m.quoted.text) text = m.quoted.text;
-  try {
-    const result = await translate(`${text}`, {to: lang, autoCorrect: true});
-    await m.reply(tradutor.texto3 + result.text);
-  } catch {
-    try {
-      const lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=${lolkeysapi}&text=${text}`);
-      const loll = await lol.json();
-      const result2 = loll.result.translated;
-      await m.reply(`${tradutor.texto3 }` + result2);
-    } catch {
-      await m.reply(tradutor.texto2);
-    }
-  }
-};
-handler.command = /^(translate|traducir|trad)$/i;
-export default handler;
+import translate from '@vitalets/google-translate-api'
+import fetch from 'node-fetch'
+let handler = async (m, { args, usedPrefix, command }) => {
+let msg = `*هذا الأمر يمكنك من الترجمة الفورية لكل اللغات* 
+_مثال اذا اردت الترجمة للعربية_ : 
+*.tr* ar Hello`
+if (!args || !args[0]) return m.reply(msg)  
+let lang = args[0]
+let text = args.slice(1).join(' ')
+const defaultLang = 'ar'
+if ((args[0] || '').length !== 2) {
+lang = defaultLang
+text = args.join(' ')}
+if (!text && m.quoted && m.quoted.text) text = m.quoted.text
+try {      
+let result = await translate(`${text}`, { to: lang, autoCorrect: true })
+await m.reply('🍀' + result.text)
+} catch {
+try {    
+let lol = await fetch(`https://api.lolhuman.xyz/api/translate/auto/${lang}?apikey=85faf717d0545d14074659ad&text=${text}`)
+let loll = await lol.json()
+let result2 = loll.result.translated
+await m.reply('*تفضل هذه ترجمتك سيدي \:* ' + result2)
+} catch { 
+await m.reply('*مشكلة أعد المحاولة لاحقا *')    
+}}}
+handler.help = ["translate"]
+handler.tags = ["tools"]
+handler.command = /^(translate|t)$/i
+export default handler
